a08972a128ae5c118bb7c5f837c9aebd0d760706
engamany <76686674+engamany@users.noreply.github.com>
2021-08-03 18:07:18 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,65 +1,182 @@
 <?php
-# task1
-
-$hexMessage = '596f7520636f6e76657274656420697420636f72726563746c7921';
-echo pack("H*",$hexMessage);
-
+//error_reporting(0);
+//task1
+echo "task1";
 echo "<br>";
 echo "<br>";
 
-// task2
+$names=['Ahmed','hassan','Eslam','Amany','Alaa'];
 
+foreach($names as $name){
 
-$htmlContent = "<h1>PHP track</h1><p>PHP is an interpreted language</p>";
-echo strip_tags($htmlContent);
-
+}
+echo max($names);
 echo "<br>";
 echo "<br>";
 
+//task2
+
+echo "task2";
+echo "<br>";
+echo "<br>";
+
+  $myinfo = ["Amany","reda","amany@gmail.com","Meto","Female","2021-08-2"];
+
+     foreach ($myinfo as $k => $v) {
+     
+          unset($myinfo[3]);
+          $myinfoo = array_values($myinfo);
+        }
+
+        echo "<pre>";
+        print_r($myinfo);
+        print_r($myinfoo);
+       echo "</pre>";
 
 //task3
-
-$username = "  Kareem Fouad ";
-
-echo trim($username);
+echo "task3";
 echo "<br>";
 echo "<br>";
+
+$salaries =[1.2,4,15555.5,352789253.7,7.5];
+foreach($salaries as $value){
+  $x=round($value);
+echo number_format($x),"<br>";
+}
+
 
 //task4
-$num = 15023198.91;
-
-echo number_format($num,2);
+echo "task4";
 echo "<br>";
 echo "<br>";
 
+$AllChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+-/=+-_)(&\|^%$#@!~`}]{[/';:><?.";
+    print_r(substr(str_shuffle($AllChars),0,20));
 
+
+echo "<br>";
+echo "<br>";
 //task5
-$employeePositions = [
-    'kareem fouad' => 'backend',
-    'ahmed bahnasy' => 'frontend',
-    'mohammed nabeel' => 'android',
-];
-print_r (array_keys($employeePositions));
+echo "task5";
 echo "<br>";
 echo "<br>";
-print_r (array_values($employeePositions));
+
+$Pronouns = array("I", "He", "She", "It",
+             "We", "They", "You");
+$n = count($Pronouns) ;
+   for($i = $n ; $i >= 0; $i--)
+    {
+        $j = rand(0, $i+1);
+ 
+        $tmp = $Pronouns[$i];
+        $Pronouns[$i] = $Pronouns[$j];
+        $Pronouns[$j] = $tmp;
+    }
+    for($i = 0; $i < $n+1; $i++){
+    echo $Pronouns[$i]." ";
+
+}
 echo "<br>";
 echo "<br>";
 
 //task6
-$nums = [4, 7, 1];
-list($x,$y,$z)=$nums;
-echo "x=$x, y=$y, z=$z";
+echo "task6";
 echo "<br>";
 echo "<br>";
 
-//task7
-$userData = [
-    'name' => 'kareem',
-    'job' => 'backend',
-    'language' => 'php',
-    'framework' => 'laravel',
-];
-extract($userData);
-echo "\$name=$name ;\$job=$job;\$language=$language;\$framework=$framework";
+$n =2082000;
+$count = 0;
+    while ($n != 0)
+    {
+        $n = round($n / 10);
+        ++$count;
+    }
+   echo $count;
+
+echo"<br>";
+echo"<br>";
+
 ?>
+
+
+
+<!-- task7 -->
+<?php 
+echo "task7";
+echo "<br>";
+echo "<br>";
+$engs = ['ahmed', 'mohammed', 'hasan'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <?php 
+        foreach($engs as $eng) { 
+            ?>
+            <li><?= $eng?></li>
+            <?php } ?>
+    
+    </ul>
+
+    
+</body>
+</html>
+
+<?php
+//task8
+echo "task8";
+echo "<br>";
+echo "<br>";
+$instructors = [
+    'kareem fouad' => [
+        'job' => 'backend developer',
+        'track' => 'php',
+    ],
+    'ahmed bahnasy' => [
+        'job' => 'frontend developer',
+        'track' => 'angular',
+    ],
+    'ahmed nasr' => [
+        'job' => 'backend developer',
+        'track' => '.net',
+    ],
+];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <?php 
+        foreach($instructors as $key=> $value){
+            foreach($value as $k=> $v){
+
+        ?>
+         
+         <li><?= $key ?>
+         <ul>
+         <li><?= $k.':'.$v ?></li>
+         </ul>
+        </li>
+       
+        <?php }?>
+        <?php }?>
+    </ul>
+    
+</body>
+</html>
+
